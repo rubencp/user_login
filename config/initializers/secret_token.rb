5af5d70bc64332b7b7948460770e9d8674864b7c
Ruben-Lap <yo@lap.samsung>
2014-01-07 13:17:02 -0600

    Initial commit + mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LogUsuarios::Application.config.secret_key_base = 'bd59267b2f4130c7490aab6408efca34b19ffd5492051576908caf07e09cf9ce65a9d2e0df109af1d56a952863c7ca41772dbae23943b6a3758f5b10d01beafc'
